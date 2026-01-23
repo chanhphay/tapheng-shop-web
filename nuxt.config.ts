356e050e39ee5bca46149d0b3d138096d5861b1b
chanhphay <chanhphay96@gmail.com>
2026-01-23 15:30:50 +0700

    Change deployment preset to Cloudflare Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
   },
   css: ['~/assets/css/fonts.css'],
   nitro: {
-    preset: 'netlify',
+    preset: 'cloudflare-pages',
     prerender: {
       crawlLinks: true,
       routes: ['/']
