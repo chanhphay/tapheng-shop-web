64646ff77d411e3e695df377892dee8aca7a6561
chanhphay <chanhphay96@gmail.com>
2026-01-30 17:09:52 +0700

    Move Phetsarath-OT font to assets and update CSS to use local font

--- app/app.vue
+++ app/app.vue
@@ -9,12 +9,12 @@
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Lao:wght@400;500;600;700&display=swap');
 
 * {
-  font-family: 'Noto Sans Lao', Arial, sans-serif;
+  font-family: 'Phetsarath-OT', 'Noto Sans Lao', Arial, sans-serif;
 }
 
 body {
   margin: 0;
   padding: 0;
-  font-family: 'Noto Sans Lao', Arial, sans-serif;
+  font-family: 'Phetsarath-OT', 'Noto Sans Lao', Arial, sans-serif;
 }
 </style>
